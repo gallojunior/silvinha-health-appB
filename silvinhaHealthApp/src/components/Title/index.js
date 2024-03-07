bb374566663d72145118206e582be0c9990478a8
GalloJunior <gallojunior@gmail.com>
2024-03-07 11:37:14 -0300

    Codificação e estilização do componente Form e criação do componente ResultImc

---
+++ silvinhaHealthApp/src/components/Title/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+export default function Title() {
+  return (
+    <View style={styles.boxTitle}>
+      <Text style={styles.textTitle}>
+        Silvinha Health App
+      </Text>
+    </View>
+  )
+}
